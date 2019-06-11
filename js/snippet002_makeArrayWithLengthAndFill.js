0b4f88f81b0303494161c809cff39370a1d3ce1a
kheradmand <kheradmand.v1@gmail.com>
2019-06-11 11:39:41 +0430

    define an array with fixed length and fill with string

---
+++ js/snippet002_makeArrayWithLengthAndFill.js
@@ -0,0 +1,12 @@
+// Create an array with predefined length
+// and fill it with values
+
+const basket = new Array(3);
+// basket : [undefined, undefined, undefined]
+
+basket.fill('apple');
+// basket : ["apple", "apple", "apple"]
+
+
+// to copy this snippet, go to:
+// github.com/developerium/useful-code-snippets
