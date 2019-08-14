3582d6de5a7a1e45332f939f3519c2005317fe0d
kheradmand <kheradmand.v1@gmail.com>
2019-08-14 13:00:49 +0430

    js: Window width and height info

---
+++ js/snippet010_windowWidthAndHeight.js
@@ -0,0 +1,18 @@
+// js#10 Window width and height info
+
+console.log(window.innerWidth); // width
+console.log(window.innerHeight); // height
+
+// will be called whenever window size changes
+window.addEventListener(
+  'resize',
+  () => {
+    // grab changed value here
+
+    console.log(window.innerWidth);
+    console.log(window.innerHeight);
+  }
+);
+
+// to copy this snippet, go to:
+// github.com/developerium/useful-code-snippets
