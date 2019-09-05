41b59f74270cb264482fc2d8600c8b2fec3a7aab
kheradmand <kheradmand.v1@gmail.com>
2019-09-05 15:36:36 +0430

    js: Operation duration

---
+++ js/snippet014_operationDuration.js
@@ -0,0 +1,15 @@
+// js#14 Operation duration
+// You can track how much time a certain task
+// has taken to complete by marking start, and
+// end of task
+
+console.time('someWork');
+
+// do some time-sensitive work
+// ...
+
+console.timeEnd('someWork');
+// will log => someWork: 14567.882080078125ms
+
+// to copy this snippet, go to:
+// github.com/developerium/useful-code-snippets
